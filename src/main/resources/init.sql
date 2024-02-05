ce55146010a2819bd7cee6ced29846cd57de0db1
Luiz Rodolfo Scudeller <rodolfoscudeller@gmail.com>
2024-02-05 17:12:45 -0300

    feat: add init.sql for database initialization

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,15 +1,15 @@
-create database api_usuarios_teste;
+create database if not exists api_usuarios_teste;
 use api_usuarios_teste;
-create table usuarios (
-    cpf varchar(100) primary key,
-    nome varchar(100) not null,
-    ativo boolean,
-    criadoEm datetime,
-    atualizadoEm datetime
+create table if not exists usuarios (
+  cpf varchar(100) primary key,
+  nome varchar(100) not null,
+  ativo boolean,
+  criadoEm datetime,
+  atualizadoEm datetime
 );
-create database api_usuarios;
+create database if not exists api_usuarios;
 use api_usuarios;
-create table usuarios (
+create table if not exists usuarios (
   cpf varchar(100) primary key,
   nome varchar(100) not null,
   ativo boolean,
